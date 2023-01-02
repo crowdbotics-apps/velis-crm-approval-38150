cc33d674c81132429174cf4eb9e1ccdf943d59fc
Crowdbotics <team@crowdbotics.com>
2023-01-02 09:25:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "velis_crm_approval_38150",
+  url: "https://velis_crm_approval_38150.botics.co",
+  api: "https://velis_crm_approval_38150.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
